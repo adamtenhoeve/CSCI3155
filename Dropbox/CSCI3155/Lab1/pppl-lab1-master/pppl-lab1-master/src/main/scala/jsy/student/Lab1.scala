9aa488e272ea73f510cbe7e9e56197f5ba6cdd3d
adamtenhoeve <32109781+adamtenhoeve@users.noreply.github.com>
2018-01-28 16:58:10 -0700

    Updated with comments

--- Dropbox/CSCI3155/Lab1/pppl-lab1-master/pppl-lab1-master/src/main/scala/jsy/student/Lab1.scala
+++ Dropbox/CSCI3155/Lab1/pppl-lab1-master/pppl-lab1-master/src/main/scala/jsy/student/Lab1.scala
@@ -82,30 +82,30 @@ object Lab1 extends jsy.util.JsyApplication with jsy.lab1.Lab1Like {
 
   def sqrtStep(c: Double, xn: Double): Double = {
     require(c > 0) 
-    xn - ((xn*xn-c) / (2 * xn))
+    xn - ((xn*xn-c) / (2 * xn)) // Computes the square root from the square root formula
   }
 
-  def sqrtNhelper(c: Double, x: Double, n: Int) :Double = {
+  def sqrtNhelper(c: Double, x: Double, n: Int) :Double = { // Helper function for sqrtN
     if (n > 0)
-      sqrtNhelper(c, sqrtStep(c, x), n-1)
-    else
-      x
+      sqrtNhelper(c, sqrtStep(c, x), n-1) // Recursively calls the helper function with n-1 as iterations so it executes
+    else                                  // the sqrtStep function a total of n times
+      x     // When done executing, returns the final calculated value
   }
 
   def sqrtN(c: Double, x0: Double, n: Int): Double = {
     require(c > 0)
-    require(n >= 0)
-    if (n == 0) x0
-    sqrtNhelper(c, x0, n)
+    require(n >= 0) // will not run if negative iterations
+    if (n == 0) x0 // returns value if no iterations
+    sqrtNhelper(c, x0, n) // calls helper function n times, which returns calculated value
   }
 
-  def sqrtErr(c: Double, x0: Double, epsilon: Double): Double = {
-    require(epsilon > 0)
-    if (abs((x0 * x0) - c) >= epsilon)
+  def sqrtErr(c: Double, x0: Double, epsilon: Double): Double = { // Calculates sqrt until within certain error
+    require(epsilon > 0) // Exits once within certain error. Stops function from running infinitely
+    if (abs((x0 * x0) - c) >= epsilon) // If not within the error, recursively calls the function again with calculated parameters
     {
-      sqrtErr(c, sqrtStep(c, x0), epsilon)
+      sqrtErr(c, sqrtStep(c, x0), epsilon) // Uses sqrtStep to calculate square root after each iteration
     }
-    else
+    else // returns final value
     {
       x0
     }
@@ -124,79 +124,80 @@ object Lab1 extends jsy.util.JsyApplication with jsy.lab1.Lab1Like {
   // case object Empty extends SearchTree
   // case class Node(l: SearchTree, d: Int, r: SearchTree) extends SearchTree
 
-  def repOk(t: SearchTree): Boolean = {
-    def check(t: SearchTree, min: Int, max: Int): Boolean = t match {
-      case Empty => true
-      case Node(l, d, r) => if (d >= min && d < max)
-        check(l, min, d) && check (r, d, max)
+  def repOk(t: SearchTree): Boolean = { // Function to see if tree is valid
+    def check(t: SearchTree, min: Int, max: Int): Boolean = t match { // checks to see if each node is valid
+      case Empty => true // Returns if empty node
+      case Node(l, d, r) => if (d >= min && d < max) // if is true if node is correct in tree, no problems
+        check(l, min, d) && check (r, d, max) // calls on children with updated parameters
       else
-        false
+        false // returns false if tree has some incorrect parameters
     }
     check(t, Int.MinValue, Int.MaxValue)
   }
 
-  def insert(t: SearchTree, n: Int): SearchTree = t match{
-    case Empty => Node(Empty, n, Empty)
-    case Node(l, d, r) =>
-      if (n < d) Node(insert(l, n), d, r)
-      else Node(l, d, insert(r, n))
+  def insert(t: SearchTree, n: Int): SearchTree = t match{ // Function to add a node to bottom of tree
+    case Empty => Node(Empty, n, Empty) // If bottom of tree, adds node to bottom
+    case Node(l, d, r) => // If not bottom, then must traverse tree
+      if (n < d) Node(insert(l, n), d, r) // If n < d, goes down left subtree. Always returns a node
+      else Node(l, d, insert(r, n)) // else traverses down right subtree
   }
 
-  def deleteMin(t: SearchTree): (SearchTree, Int) = {
+  def deleteMin(t: SearchTree): (SearchTree, Int) = { // Function to find minimum node of right subtree
     require(t != Empty)
     (t: @unchecked) match {
-      case Node(Empty, d, r) => (r, d)
-      case Node(l, d, r) =>
-        val (l1, m) = deleteMin(l)
-        (Node(l1, d, r), m)
+      case Node(Empty, d, r) => (r, d) // If no left child, then is minimum value and returns that value and right tree of that value
+      case Node(l, d, r) => // If not minimum value, continues to traverse right.
+        val (l1, m) = deleteMin(l) // l1 is right subtree of minimum value, m is minimum value
+        (Node(l1, d, r), m) // returns the touple of the tree with that value replaced and the value that was deleted
     }
   }
 
-  def delete(t: SearchTree, n: Int): SearchTree =
+  def delete(t: SearchTree, n: Int): SearchTree = // Function to delete any node on the tree
   {
     (t: @unchecked) match {
       case Node(Empty, d, Empty) => // No children
-        if (n != d)
-          t
-        else
+        if (n != d) // If not equal to the value, then not in tree
+          t // Returns full tree
+        else // Node to be deleted has no children as can be set to Empty
           Empty
       case Node(l, d, Empty) => // Only left child
-        if (n == d)
-          l
-        else
-          Node(delete(l, n), d, Empty)
+        if (n == d) // If value
+          l // Set Node as left subtree
+        else // Not equal to value
+          Node(delete(l, n), d, Empty) // Continue traversing left
       case Node(Empty, d, r) => // Only right child
-        if (n == d) {
-          r
+        if (n == d) { // if value
+          r // Overwrite Node with right subtree
         }
-        else
-          Node(Empty, d, delete(r, n))
+        else // Not value
+          Node(Empty, d, delete(r, n)) // Traverse down right subtree
       case Node(l, d, r) => // Two children
-        if (n == d)
+        if (n == d) // if value
         {
-          val (tree, value) = deleteMin(r)
-          Node(l, value, tree)
+          val (tree, value) = deleteMin(r) // call deleteMin to find minimum value of right subtree
+          Node(l, value, tree) // Sets returns current node as the minimum value, deleting current node
         }
-        else if (n < d)
+        else if (n < d) // If n is less than current node, traverses down left subtree
           Node(delete(l, n), d, r)
-        else
+        else  // If greater than, traverses down right subtree.
           Node(l, d, delete(r, n))
     }
   }
 
   /* JavaScripty */
 
-  def eval(e: Expr): Double = e match {
-    case N(n) => n
+  def eval(e: Expr): Double = e match { // Basic calculator function to calculate simple expressions
+    case N(n) => n // If expression is a number, returns the number
       n
-    case Unary(uop, e1) => uop match {
-      case Neg => -1 * eval(e1)
+    case Unary(uop, e1) => uop match { // If expression is a single operator, returns the operator
+      case Neg => -1 * eval(e1) // Only operator is negative so multiplies the value by -1
     }
-    case Binary(bop, e1, e2) => bop match {
-      case Plus => eval(e1) + eval(e2)
-      case Minus => eval(e1) - eval(e2)
-      case Times => eval(e1) * eval(e2)
-      case Div => eval(e1) / eval(e2)
+    case Binary(bop, e1, e2) => bop match { // If expression is binary, case matches which binary expression and performs calulation
+      case Plus => eval(e1) + eval(e2) // addition
+      case Minus => eval(e1) - eval(e2) // subtraction
+      case Times => eval(e1) * eval(e2) // multiplication
+      case Div => eval(e1) / eval(e2) // division
+                                      // Calls eval on each subexpression in case abstract tree is longer
     }
   }
 
